00ee7a8b59631ea5d68703b0dd73ac164ad91eeb
Jose Fernandez <jose@umn.edu>
2008-09-25 15:51:56 -0500

    Comment out this line of the test helper requires the plugin to be part of a rails app...

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-require "#{File.expand_path(File.dirname(__FILE__) + "/../../../../")}/config/boot.rb"
+#require "#{File.expand_path(File.dirname(__FILE__) + "/../../../../")}/config/boot.rb"
 require 'rubygems'
 require 'spec'
 require 'active_support'
